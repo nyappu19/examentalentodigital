40bd5e82131cab7c180976165019a359181c82a2
nyappu19 <nyappu@gmail.com>
2023-07-14 11:03:23 -0400

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <v-app>
-    <nav-bar></nav-bar>
+    <NavBar></NavBar>
     <v-main>
       <router-view/>
     </v-main>
+<Footer></Footer>
   </v-app>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import Footer from './components/Footer.vue';
+
+
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
-  components:{
-    'nav-bar':NavBar
-  }
+  components: {
+    NavBar,
+    Footer
+   
+  },
 };
 </script>
